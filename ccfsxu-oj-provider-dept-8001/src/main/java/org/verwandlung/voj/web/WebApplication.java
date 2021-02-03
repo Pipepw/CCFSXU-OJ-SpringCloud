b10c15f592bfd2dfb3b74724e6ed1acb977956e5
Pipepw <416361536@qq.com>
2021-02-03 17:24:50 +0800

    feat(eureka): 启动类新增自动注册到Eureka以及服务发现的相关注解

--- ccfsxu-oj-provider-dept-8001/src/main/java/org/verwandlung/voj/web/WebApplication.java
+++ ccfsxu-oj-provider-dept-8001/src/main/java/org/verwandlung/voj/web/WebApplication.java
@@ -2,9 +2,12 @@ package org.verwandlung.voj.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.jms.annotation.EnableJms;
 
-
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //服务发现
 @SpringBootApplication
 @EnableJms  //启动Jms，替代了之前在下面写的Bean
 public class WebApplication {
